26b32a05692e31740ca0c537cb25ca7af9509c92
seoyeon83 <mool848777@naver.com>
2023-01-08 22:21:49 +0900

    [23.01.08] BOJ Python 배우기 문제집

---
+++ BOJ/Python/BOJ_1789.py
@@ -0,0 +1,9 @@
+# 푸는 중~
+
+S = int(input())
+sum = 0
+
+for i in range(4294967295) :
+    pass
+
+print(i, sum)
---
+++ BOJ/Python/BOJ_2935.py
@@ -0,0 +1,8 @@
+A = int(input())
+d = input()
+B = int(input())
+
+if d == "+" :
+    print(A + B)
+elif d == "*" :
+    print(A * B)
