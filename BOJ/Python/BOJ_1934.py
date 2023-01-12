5b04a0e50ccaf7fa32eb87fd1178229db8ad1422
seoyeon83 <mool848777@naver.com>
2023-01-12 19:18:40 +0900

    [23.01.12] BOJ Python 배우기 문제집

--- BOJ/Python/BOJ_1934.py
+++ BOJ/Python/BOJ_1934.py
@@ -1,14 +1,26 @@
-### 푸는 중~
 ## 1
+# 시간초과로 실패
+# import sys
 # for _ in range(int(input())) :
-#     A, B = sorted(map(int, input().split()))
+#     A, B = sorted(map(int, sys.stdin.readline().split()))
 #     i = B//A
-#     print(A, B, i)
+#
 #     while (A*i % B != 0) :
 #         i += 1
 #
 #     print(A*i)
 
+## 1
+# A, B 최대공약수 * 최소공배수 = A * B
+# 성공! 하지만 4260ms가 걸린다... 쩝~
+
 for _ in range(int(input())) :
     A, B = sorted(map(int, input().split()))
+    G = A
 
+    for g in range(A, 0, -1) :
+        if ((A%g == 0) and (B%g == 0)) :
+            G = g
+            break
+
+    print(A * B // G)
