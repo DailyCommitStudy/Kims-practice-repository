4d9d05f9c5c2771deecca4e4366a947af9a0e5e0
seoyeon83 <mool848777@naver.com>
2023-02-02 20:59:26 +0900

    [23.02.02] not solving

--- BOJ/Python/초등학교를 입학하자 - KOI편/BOJ_2606.py
+++ BOJ/Python/초등학교를 입학하자 - KOI편/BOJ_2606.py
@@ -2,18 +2,33 @@
 # a: 감염된 애들(1과 같은 쌍이거나 감염된 애들과 같은 쌍인 경우 add)
 # 연결된 애들 연결고리를 만들고, 반복문을 다시 돌면서 판단
 
-case = list()
-v = {1}
-N = int(input())
-for i in range(int(input())) :
-    case.append(list(map(int, input().split(' '))))
-    if case[i][0] in v : v.add(case[i][1])
-    elif case[i][1] in v : v.add(case[i][0])
+# 컴퓨터별로 연결된 애들을 모두 set으로 만들어두고(리스트) 마지막에 1과 연결된 애들을 set으로 합치고 len.
 
-for c in range(len(case)-1, -1, -1) :
-    if case[c][0] in v : v.add(case[c][1])
-    elif case[c][1] in v : v.add(case[c][0])
+# case = list()
+# v = {1}
+# N = int(input())
+# for i in range(int(input())) :
+#     case.append(list(map(int, input().split(' '))))
+#     if case[i][0] in v : v.add(case[i][1])
+#     elif case[i][1] in v : v.add(case[i][0])
+#
+# for c in range(len(case)-1, -1, -1) :
+#     if case[c][0] in v : v.add(case[c][1])
+#     elif case[c][1] in v : v.add(case[c][0])
+#
+#
+# print(len(v)-1)
 
-
-print(len(v)-1)
-
+##
+# N = int(input())
+# case = {i:set() for i in range(1,N+1)}
+# for i in range(int(input())) :
+#     a, b = map(int, input().split())
+#     case[a].add(b)
+#     case[b].add(a)
+#
+# res = case[1].copy()
+# for v in case.values() :
+#     if 1 in v : res.update(v)
+#
+# print(len(res)-1)
