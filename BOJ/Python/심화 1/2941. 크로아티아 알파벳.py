c5cf2d6d939f1989e9c23b4034252bbd98de2a84
seoyeon83 <mool848777@naver.com>
2024-01-07 15:30:32 +0900

    [24.01.07] success

--- BOJ/Python/심화 1/2941. 크로아티아 알파벳.py
+++ BOJ/Python/심화 1/2941. 크로아티아 알파벳.py
@@ -1,3 +1,9 @@
-alp = ["c=", "c-", "dz=", "lj", "nj", "s=", "z="]
+# 크로아티아 알파벳을 특수기호로 매핑 후, 문자열의 길이가 정답
 
 str = input()
+
+# 크로아티아 알파벳 replace
+for s in ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]:
+    str = str.replace(s, ".")
+
+print(len(str))
