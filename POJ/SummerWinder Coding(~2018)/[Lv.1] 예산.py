0dc7fb96edf5e744d1e26b3023575bdc2731d8f5
seoyeon83 <mool848777@naver.com>
2024-02-23 21:52:42 +0900

    [24.02.23] solving

--- POJ/SummerWinder Coding(~2018)/[Lv.1] 예산.py
+++ POJ/SummerWinder Coding(~2018)/[Lv.1] 예산.py
@@ -9,3 +9,21 @@ def solution(d, budget):
         for j in cb(d, i):
             if sum(j) <= budget:
                 return i
+
+# 다른 사람 코드 참고해서 푼 것
+# 솔직히 아직까지 정확히 이해가 안됨.ㅠㅠ 이게 모든 경우를 포괄할 수 있어 ㄹㅇ로?
+
+# 새로운 로직?
+# 모든 부서에 대해 작은 예산부터 빼본다
+# 이때 마이너스가 되면 return한다
+
+def solution(d, budget):
+    d.sort()
+
+    for i, n in enumerate(d):
+        budget -= n
+
+        if budget < 0:
+            return i
+
+    return len(d)
